a2aaee4af9d010dd0eeb5501553f630182fedf72
Drew Smith <andrew.j.smith.jr@gmail.com>
2017-09-16 01:39:41 -0500

    removing redux remnants

--- src/actions/ActionTypes.js
+++
@@ -1,1 +0,0 @@
-export const ADD_TO_CATEGORY = 'ADD_TO_CATEGORY'
--- src/actions/Actions.js
+++
@@ -1,3 +0,0 @@
-import * as types from './ActionTypes'
-
-export const addToCategory = bookId => ({ type: types.ADD_TO_CATEGORY, bookId })
